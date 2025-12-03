8f2667b2a490715ff9fed08b50ee8ded975e2a3b
makar10l <makar10l.mail@proton.me>
2025-12-03 22:35:52 +0300

    creating logo

--- src/lib/files/src/create.rs
+++ src/lib/files/src/create.rs
@@ -1,8 +1,10 @@
+use std::io::Write;
 pub fn create_data(){
     let user = std::env::var("USER").expect("Error with getting username");
     exits(std::fs::create_dir(format!("/home/{}/.rustify", user)));
-    exits(std::fs::create_dir(format!("/home/{}/.rustify/data", user)));
+    exits(std::fs::create_dir(format!("/home/{}/.rustify/data", user));
     exits(std::fs::create_dir(format!("/home/{}/.rustify/bin", user)));
+    logo(format!("/home/{}/.rustify/data/logo.txt", user));
     std::fs::copy("/bin/.main_rustify", format!("/home/{}/.rustify/bin/rustify", user)).expect("Error: cannot move bin");
 }
 
@@ -16,4 +18,13 @@ fn exits(result : Result<(), std::io::Error>){
         },
         Ok(_) => (),
     };
+}
+fn logo(path : &str) -> Result<(), std::io::Error>{
+    let mut file = match std::fs::File::create(path){
+        Err(err) => {println!("Cannot create logo.txt"); return Err(err)},
+        Ok(descriptor) => descriptor, 
+    };
+    let logo = std::fs::read_to_string("data/logo.txt").expect("Cannot read logo");
+    write!(file, "{}",logo);
+    Ok(())
 }
